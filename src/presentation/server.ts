b07aeafbb59e64a330a0420288c1e1c082886188
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2025-03-25 19:08:40 -0300

    2. Video 111 Finalizado

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,18 @@
+import { CronJob } from "cron";
+
 export class Server {
     public static start() {
         console.log('Server started...');
+
+        const job = new CronJob(
+            '*/2 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('You will see this message every two seconds.', date);
+            },
+        );
+
+        job.start();
     }
+
 }
