641884601744ecc519e6d076f95499056c18c1be
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2025-03-27 12:25:08 -0300

    5. Video 125 Finalizado

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -7,11 +7,19 @@ export enum LogSeverityLevel {
 export class LogEntity {
     public level: LogSeverityLevel;
     public message: string;
-    public date: Date;
+    public createdAt: Date;
 
     constructor(message: string, level: LogSeverityLevel){
         this.message = message;
         this.level = level;
-        this.date = new Date();
+        this.createdAt = new Date();
+    }
+
+    static fromJson = (json: string): LogEntity => {
+        const {message, level, createdAt} = JSON.parse(json);
+        const log = new LogEntity(message, level);
+        log.createdAt = new Date(createdAt);
+
+        return log;
     }
 }
