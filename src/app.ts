a5290833a20d7b218fc9d16e47bbe5dedf5aaafb
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2025-03-28 12:59:03 -0300

    11. Video 131 Finalizado

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/plugins/envs.plugin";
 import { Server } from "./presentation/server";
 
 (async() => {
@@ -5,5 +6,10 @@ import { Server } from "./presentation/server";
 })();
 
 function main() {
-    Server.start();
+    // Server.start();
+    console.log(envs.PORT);
+    console.log(envs.MAILER_EMAIL);
+    console.log(envs.MAILER_SECRET_KEY);
+    console.log(envs.PROD);
+    console.log(envs);
 }
